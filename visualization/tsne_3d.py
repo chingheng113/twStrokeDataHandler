09926a516ee958d4f080cb83e1bf06b9fda2432c
aalinc9 <chingheng113@gmail.com>
2018-08-31 09:58:26 -0400

    t-sne visualization 2D, 3D

--- visualization/tsne_3d.py
+++ visualization/tsne_3d.py
@@ -3,6 +3,7 @@ import numpy as np
 import pandas as pd
 from tools import genral_utils
 import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 
 
 if __name__ == '__main__':
@@ -11,19 +12,19 @@ if __name__ == '__main__':
 
     if n_class == 2:
         id_data, x_data, y_data = genral_utils.get_poor_god('wholeset_Jim_nomissing_validated.csv')
-        fn = 'reduced_dimension_30_2c'
+        fn = 'reduced_dimension_30_2c_3D'
     else:
         id_data, x_data, y_data = genral_utils.get_individual('wholeset_Jim_nomissing_validated.csv')
-        fn = 'reduced_dimension_30_individual'
+        fn = 'reduced_dimension_30_individual_3D'
     # calculation
     # x_data_train = genral_utils.scale(x_data)
-    # t_sne = TSNE(n_components=2, perplexity=30).fit_transform(x_data_train)
-    # df = pd.DataFrame(t_sne, columns=['x', 'y'])
+    # t_sne = TSNE(n_components=3, perplexity=30).fit_transform(x_data_train)
+    # df = pd.DataFrame(t_sne, columns=['x', 'y', 'z'])
     # df['p'] = y_data.values
     # genral_utils.save_dataframe_to_csv(df, fn)
 
     df = pd.read_csv(genral_utils.get_file_path(fn+'.csv', under_raw=False), encoding='utf8')
-    plt.figure()
-    plt.scatter(df.ix[:,0], df.ix[:,1], c=df.ix[:, 2], s=0.1, cmap=plt.cm.get_cmap("jet", n_class))
-    plt.colorbar(ticks=range(n_class))
+    fig = plt.figure()
+    ax = Axes3D(fig)
+    ax.scatter(df.ix[:,0], df.ix[:,1], df.ix[:,2], c=df.ix[:, 3], s = 0.1, cmap=plt.cm.get_cmap("jet", n_class))
     plt.show()
